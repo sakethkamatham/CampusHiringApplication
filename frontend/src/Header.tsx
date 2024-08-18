c3cd5f9045069b0d5c4b932d4b4986db28616a99
Saketh Kamatham <saketh@Sakeths-MacBook-Pro.local>
2024-08-18 00:38:04 -0400

    Frontend Commit. Basic Code

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Campus Hiring Application</h1>
+    </header>
+  );
+};
+
+export default Header;
